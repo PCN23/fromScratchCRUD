1aca1c3b5985fe6cfbe65756b9a6756ad7f4712e
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-27 11:21:39 -0700

    adding handle submit

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -7,16 +7,20 @@ export default function AuthPage() {
   const [email, setEmail] = useState ('');
   const [password, setPassword] = useState ('');
 
+git ad
 
-  
   return (
     <div className='forms'>
-      <form>
-        <lable> email <input type="email"/>
+      <form onSubmit={handleSubmit}> 
+        {/* the handle submit will be an async function, gonna call supabase. */}
+        <lable> email 
+          {/* this is a controlled input.
+            whatever shows up here becomes the state */}
+          <input value={email} type="email"/> 
         </lable>
       </form>
       <form>
-        <lable> password <input type="password"/>
+        <lable> password <input value={password} type="password"/>
         </lable>
         <button>Sign up!</button>
       </form>
