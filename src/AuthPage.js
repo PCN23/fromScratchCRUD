be33fb78b57b04c363820e2c0ba7b18641beead7
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-27 11:13:38 -0700

    added state

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 
+
+
+//after making form, we need to make state, something that we expect to change.
 export default function AuthPage() {
+  const [email, setEmail] = useState ('');
+  const [password, setPassword] = useState ('');
+
+
+  
   return (
-    <div>
+    <div className='forms'>
       <form>
         <lable> email <input type="email"/>
         </lable>
@@ -12,6 +20,16 @@ export default function AuthPage() {
         </lable>
         <button>Sign up!</button>
       </form>
+      <form>
+        <lable> email <input type="email"/>
+        </lable>
+      </form>
+      <form>
+        <lable> password <input type="password"/>
+        </lable>
+        <button>Sign in!</button>
+      </form>
+        
         
         
     </div>
