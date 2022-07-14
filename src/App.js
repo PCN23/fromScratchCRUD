3bde3acaafe7b25cbb9e442f47e5a139c8677cd0
Pedro <pedro.chn01@gmail.com>
2022-07-14 12:47:29 -0700

    starting on auth page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import ListPage from './ListPage';
+import AuthPage from './AuthPage';
+import CreatePage from './CreatePage';
+import UpdatePage from './UpdatePage';
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn How to finish this
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <AuthPage />
+          </Route>
+          <Route exact path="/movies">
+            <ListPage />
+          </Route>
+          <Route path="/create">
+            <CreatePage />
+          </Route>
+          <Route path="/movies/:id">
+            <UpdatePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
